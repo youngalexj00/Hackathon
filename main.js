515ba915816fec524e33a24ae38bc29b592ca6e2
Kevin <ruan8111@gmail.com>
2020-06-03 21:27:43 -0400

    1

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+let googleLogo = document.getElementById('hplogo');
+let parent = x.parentNode;
+// x.parentNode.removeChild(x);
+
+let img = document.createElement('img')
+img.src = "https://lh3.googleusercontent.com/proxy/h2QJevU7_PcXTbi4Kp6oaQ9HBkSLi1FzI4T9weFksbPOgbl_rCdLs3y45wtMMAHYrcKey7iVSEiPb2IQpw"
+parent.appendChild(img)
